a82c2e2dd7c7316e8fbb0c80e9630e234193f7cc
snouz <snooz-x@hotmail.com>
2025-01-21 14:57:59 +0100

    data processor graphics

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -194,7 +194,7 @@ data:extend({
     max_health = 300,
     corpse = "assembling-machine-1-remnants",
     dying_explosion = "assembling-machine-1-explosion",
-    icon_draw_specification = {shift = {0, -0.3}},
+    icon_draw_specification = {shift = {0, 0}, scale = 0.7},
     resistances =
     {
       {
@@ -206,59 +206,147 @@ data:extend({
     {
       {
         production_type = "input",
-        pipe_picture = assembler3pipepictures(),
-        pipe_covers = pipecoverspictures(),
+        --pipe_picture = assembler3pipepictures(),
+        --pipe_covers = pipecoverspictures(),
         volume = 1000,
         filter = "raw-data",
-        pipe_connections = {{ flow_direction="input", direction = defines.direction.north, position = {0, -1}, connection_category = "raw-data"}},
+        pipe_connections = {{ flow_direction="input-output", direction = defines.direction.north, position = {0, -1}, connection_category = "raw-data"}},
         secondary_draw_orders = { north = -1 }
       },
       {
-        production_type = "output",
-        pipe_picture = assembler3pipepictures(),
-        pipe_covers = pipecoverspictures(),
+        production_type = "input",
+        --pipe_picture = assembler3pipepictures(),
+        --pipe_covers = pipecoverspictures(),
         volume = 1000,
         filter = "raw-data",
-        pipe_connections = {{ flow_direction="output", direction = defines.direction.south, position = {0, 1}, connection_category = "raw-data"}},
+        pipe_connections = {{ flow_direction="input-output", direction = defines.direction.south, position = {0, 1}, connection_category = "raw-data"}},
         secondary_draw_orders = { north = -1 }
-      }
+      },
+      {
+        production_type = "input",
+        --pipe_picture = assembler3pipepictures(),
+        --pipe_covers = pipecoverspictures(),
+        volume = 1000,
+        filter = "raw-data",
+        pipe_connections = {{ flow_direction="input-output", direction = defines.direction.east, position = {1, 0}, connection_category = "raw-data"}},
+        secondary_draw_orders = { north = -1 }
+      },
+      {
+        production_type = "input",
+        --pipe_picture = assembler3pipepictures(),
+        --pipe_covers = pipecoverspictures(),
+        volume = 1000,
+        filter = "raw-data",
+        pipe_connections = {{ flow_direction="input-output", direction = defines.direction.west, position = {-1, 0}, connection_category = "raw-data"}},
+        secondary_draw_orders = { north = -1 }
+      },
     },
     collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
     selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
     damaged_trigger_effect = hit_effects.entity(),
-    fast_replaceable_group = "assembling-machine",
-    next_upgrade = "assembling-machine-2",
+    fast_replaceable_group = "data-processor",
     circuit_wire_max_distance = assembling_machine_circuit_wire_max_distance,
     circuit_connector = circuit_connector_definitions["assembling-machine"],
     alert_icon_shift = util.by_pixel(0, -12),
     graphics_set =
     {
-      animation =
+      animation_progress = 0.25,
+      always_draw_idle_animation = true,
+      states =
+      {
+        {
+          name = "idle",
+          duration = 1,
+          next_active = "working",
+          next_inactive = "idle",
+        },
+        {
+          name = "working",
+          duration = 30,
+          next_active = "working",
+          next_inactive = "idle",
+        },
+      },
+      idle_animation =
       {
         layers =
         {
           {
-            filename = "__base__/graphics/entity/assembling-machine-1/assembling-machine-1.png",
+            filename = "__Moshine__/graphics/entity/data-processor/suit-plug-outlet-hr-base.png",
             priority="high",
-            width = 214,
-            height = 226,
-            frame_count = 32,
-            line_length = 8,
-            shift = util.by_pixel(0, 2),
+            width = 290,
+            height = 290,
+            repeat_count = 30,
+            line_length = 1,
+            animation_speed = 1/3,
+            shift = util.by_pixel(0, -16),
             scale = 0.5
           },
           {
-            filename = "__base__/graphics/entity/assembling-machine-1/assembling-machine-1-shadow.png",
+            filename = "__Moshine__/graphics/entity/data-processor/suit-plug-outlet-hr-shadow.png",
             priority="high",
-            width = 190,
-            height = 165,
+            width = 400,
+            height = 350,
+            repeat_count = 30,
             line_length = 1,
-            repeat_count = 32,
             draw_as_shadow = true,
-            shift = util.by_pixel(8.5, 5),
+            animation_speed = 1/3,
+            shift = util.by_pixel(0, -16),
             scale = 0.5
-          }
+          },
+          {
+            filename = "__Moshine__/graphics/entity/data-processor/suit-plug-outlet-hr-animation-1.png",
+            priority="high",
+            width = 160,
+            height = 290,
+            frame_count = 16,
+            line_length = 8,
+            animation_speed = 1/3,
+            run_mode = "forward-then-backward",
+            shift = util.by_pixel(0, -16),
+            scale = 0.5
+          },
+          
         }
+      },
+      working_visualisations =
+      {
+        {
+          always_draw = true,
+          draw_in_states = {"working"},
+          animation = 
+          { 
+            layers = {
+              {
+                filename = "__Moshine__/graphics/entity/data-processor/suit-plug-outlet-hr-animation-1.png",
+                priority="high",
+                width = 160,
+                height = 290,
+                frame_count = 16,
+                line_length = 8,
+                animation_speed = 1/3,
+                run_mode = "forward-then-backward",
+                shift = util.by_pixel(0, -16),
+                scale = 0.5
+              },
+              {
+                filename = "__Moshine__/graphics/entity/data-processor/suit-plug-outlet-hr-emission-1.png",
+                priority="high",
+                width = 160,
+                height = 290,
+                frame_count = 16,
+                line_length = 8,
+                animation_speed = 1/3,
+                run_mode = "forward-then-backward",
+                draw_as_glow = true,
+                blend_mode = "additive",
+                shift = util.by_pixel(0, -16),
+                scale = 0.5
+              },
+            }
+          },
+          name = "rotation"
+        },
       }
     },
     crafting_speed = 0.5,
